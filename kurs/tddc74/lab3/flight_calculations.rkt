6f6ad529840d63dccf5bf3eb641e490c895902e0
Margareta <margareta.vi@gmail.com>
2017-02-15 17:04:28 +0100

    add a non working version of flight-calculation

--- kurs/tddc74/lab3/flight_calculations.rkt
+++ kurs/tddc74/lab3/flight_calculations.rkt
@@ -4,7 +4,7 @@
 (require "prettyprinter.rkt")
 (require "flight_representation.rkt")
 
-
+(write-and-create 1000 "test-1000.db")
 (define flights
   ;; The database imported is called "test-1000.db".
   ;; If you have another database file to import, change this here!
@@ -17,3 +17,32 @@
 ;; To pretty-print a flight (human-readable), try (print-flight <flight>)
 ;; To pretty-print a flight database, try (print-db <flight database>).
 ;; Eg (print-db flights)
+
+
+(define flights-from-to 
+  (lambda (from to flight-db)
+    ;; first flight in database, flight destination, flight origin
+    (let* ((direct-flight (create-empty-database))
+           (connecting (create-empty-database))
+           (cond
+             ((empty-database? flight-db) flight-db)
+               )
+                       ;;direct flight
+                       (let direct-flight (add-to-db fir-flight direct-flight))
+                       (
+             )))))
+             
+
+             
+
+;; function that creates a db if there is a direct flight between origin and destination
+(define check-dir-flight
+  (lambda (flight-db from to)
+    (let* ((fir-flight (first-flight flight-db)) 
+           (flight-dest (flight-destination fir-flight))
+           (flight-orig (flight-origin fir-flight))
+           (if (and (eq? from flight-orig) (eq? to flight-dest))
+              (
+
+           
+           
