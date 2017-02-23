c469a36fcb48046891d7a8f7155463e332be1144
Margareta <margareta.vi@gmail.com>
2017-02-23 10:17:55 +0100

    add a not finished version of task 2

--- kurs/tddc74/lab3/flight_calculations.rkt
+++ kurs/tddc74/lab3/flight_calculations.rkt
@@ -1,10 +1,11 @@
 #lang racket
 (require racket/serialize)
+(require racket/trace)
 (require "database-creator.rkt")
 (require "prettyprinter.rkt")
 (require "flight_representation.rkt")
 
-(write-and-create 1000 "test-1000.db")
+;(write-and-create 1000 "test-1000.db")
 (define flights
   ;; The database imported is called "test-1000.db".
   ;; If you have another database file to import, change this here!
@@ -18,31 +19,40 @@
 ;; To pretty-print a flight database, try (print-db <flight database>).
 ;; Eg (print-db flights)
 
-
-(define flights-from-to 
+(define flights-from-to
   (lambda (from to flight-db)
-    ;; first flight in database, flight destination, flight origin
-    (let* ((direct-flight (create-empty-database))
-           (connecting (create-empty-database))
-           (cond
-             ((empty-database? flight-db) flight-db)
-               )
-                       ;;direct flight
-                       (let direct-flight (add-to-db fir-flight direct-flight))
-                       (
-             )))))
-             
+    (let* ((direct-flights-db (create-empty-database))
+          (connecting-flights-db (create-empty-database)))
+      (go-through-db from to flight-db
+                     direct-flights-db connecting-flights-db))))
 
-             
+(define go-through-db
+  (lambda (from to flight-db direct-flights-db connecting-flights-db)
+    (if (empty-database? flight-db)
+        (list (list 'direct-flights direct-flights-db ) (list 'connecting connecting-flights-db))
+        (let ((top-flight (first-flight flight-db)))
+          (if (direct? top-flight from to)
+              (go-through-db from to (rest-of-flights flight-db) (add-to-db flights direct-flights-db) connecting-flights-db)
+              (connecting top-flight from to flight-db
+                          direct-flights-db connecting-flights-db))))))
 
-;; function that creates a db if there is a direct flight between origin and destination
-(define check-dir-flight
-  (lambda (flight-db from to)
-    (let* ((fir-flight (first-flight flight-db)) 
-           (flight-dest (flight-destination fir-flight))
-           (flight-orig (flight-origin fir-flight))
-           (if (and (eq? from flight-orig) (eq? to flight-dest))
-              (
+;; predicate, is the flight is a direct flight?
+(define direct?
+  (lambda (flight from to)
+    (and (equal? from (flight-origin flight))
+         (equal? to (flight-destination flight)))))
 
-           
+;; if there is a connecting airport then add to the database else go to next flight
+(define connecting
+  (lambda (flight from to flight-db direct-flights-db connecting-flights-db)
+    ;;origin2 is the connecting airport
+    (let ((origin2 (flight-origin flight)))
+      (if (direct? flight origin2 to)
+          ; connecting flights exists
+          (cons flight (go-through-db from to (rest-of-flights flight-db)
+                         direct-flights-db
+                         (add-to-db flight connecting-flights-db)))
+          (go-through-db from to (rest-of-flights flight-db)
+                         direct-flights-db connecting-flights-db)))))
+
            
